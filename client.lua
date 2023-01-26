4888d0af70c79b0ba224d95b5d4b3ef643d101dd
OrangeBugatti <123387724+OrangeBugatti@users.noreply.github.com>
2023-01-25 20:22:06 -0500

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,6 +1,1 @@
-    function ENT:Use(Player)
-        if not Player:IsAdmin() then return Player:ChatPrint("You do not have permission to use this spawner.") end
-
-        local spawnPos = Player:GetPos() + Player:EyeAngles():Forward() * -100
-        Player:SendLua([[OpenVehicleSpawner(Vector("]]..tostring(spawnPos)..[[") )]])
-    end
+-undone
